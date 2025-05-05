857ffacd22c3b5ad48c18e2a70965f352b6d79c5
grigm <manon.grignon@eleves.ec-nantes.fr>
2025-05-05 13:53:56 +0200

    Modif4

--- src/app/service/texte.service.ts
+++ src/app/service/texte.service.ts
@@ -24,7 +24,7 @@ export class TexteService {
 
   // Méthode pour charger les textes depuis le fichier JSON
   loadTexts(): Observable<any> {
-    return this.http.get<any>('/assets/texts.json').pipe(); 
+    return this.http.get<any>('assets/texts.json').pipe(); 
   }
 
   // Méthode pour charger les questions depuis le fichier JSON

333430075d67e8797e11e679476938c3b0cba34c
grigm <manon.grignon@eleves.ec-nantes.fr>
2025-06-13 11:27:37 +0200

    Fixer la taille des composents et modifier about avec biblio

--- src/app/modules/about/about.component.ts
+++ src/app/modules/about/about.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, SimpleChanges } from '@angular/core';
 import { Location } from '@angular/common';
+import { DomSanitizer } from '@angular/platform-browser';
+import { FormatInfoTextPipe } from '../../pipe/format-info-text.pipe';
+import { TexteService } from '../../service/texte.service';
 
 @Component({
   selector: 'app-about',
@@ -8,8 +11,37 @@ import { Location } from '@angular/common';
   styleUrl: './about.component.less'
 })
 export class AboutComponent {
-  constructor(private location: Location){
 
+  text : any; 
+  lien1 : any; 
+  lien2 : any; 
+  lien3 : any; 
+  lien4: any; 
+
+  constructor(
+      private location: Location,
+      private texteService: TexteService,
+      private sanitizer: DomSanitizer,  // Injecter DomSanitizer
+      private formatInfoTextPipe: FormatInfoTextPipe  // Injection de ton pipe
+  ) { }
+  
+  ngOnInit(): void {
+    // Charger les questions dès le début si ce n'est pas déjà fait
+    this.texteService.loadTexts().subscribe(texts => {
+      this.texteService.setTexts(texts); 
+      this.text=texts; 
+      if (this.text.home) {
+        const formattedLien1 = this.formatInfoTextPipe.transform(this.text.home.lien1); 
+        this.lien1 = this.sanitizer.bypassSecurityTrustHtml(formattedLien1);  
+
+        const formattedLien2 = this.formatInfoTextPipe.transform(this.text.home.lien2); 
+        this.lien2 = this.sanitizer.bypassSecurityTrustHtml(formattedLien2);  
+        const formattedLien3 = this.formatInfoTextPipe.transform(this.text.home.lien3); 
+        this.lien3 = this.sanitizer.bypassSecurityTrustHtml(formattedLien3);  
+        const formattedLien4 = this.formatInfoTextPipe.transform(this.text.home.lien4); 
+        this.lien4 = this.sanitizer.bypassSecurityTrustHtml(formattedLien4);  
+      }
+    });
   }
 
   goBack():void{

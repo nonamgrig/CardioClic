5afca476233364b90300fde84f8caafa79da4c0d
grigm <manon.grignon@eleves.ec-nantes.fr>
2025-06-30 11:07:33 +0200

    Retire le dossier src du versionnement

--- src/app/pipe/format-info-text.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FormatInfoTextPipe } from './format-info-text.pipe';
-
-describe('FormatInfoTextPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FormatInfoTextPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
